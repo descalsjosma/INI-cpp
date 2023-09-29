85d1e424ac98d5f48b0936db4a660d0814f18bd3
descalsjosma <37843631+descalsjosma@users.noreply.github.com>
2023-09-29 11:27:10 +0200

    Primer programa INI

---
+++ projecte1/projecte1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << "Hello World!" << endl << "Benvinguts a l'IES Bernat Guinovart" << endl; // En esta< linia faig açò
+    cout << "I aci acava el programa" << endl;
+    return 0;
+}
